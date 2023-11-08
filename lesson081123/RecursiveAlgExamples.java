2b73cebf110c3d2f5cf135303a9929f922ac3fb5
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-11-08 11:49:00 +0200

    Update RecursiveAlgExamples.java

--- lesson081123/RecursiveAlgExamples.java
+++ lesson081123/RecursiveAlgExamples.java
@@ -1,16 +1,31 @@
 package lesson081123;
 
-public class RecursiveAlgExamples {
+public class RecurisiveAlgExamples {
+
     public static void main(String[] args) {
-        System.out.println("Result" + f1(15));
+
+        f(90);
+
+        System.out.println("Result: " + f1(15));
     }
+
     private static int f1(int n) {
-        System.out.println("f1 function"+ n);
-        if (n >= 20) return 20;
-        return f2(n+1);
+        System.out.println("f1 function: " + n);
+//        if (n == 20) return 20; // base condition
+        if (n >= 20) return 20; // base condition
+        return f2(n + 1);
     }
-    private static int f2(int n){
-        System.out.println("f2 function " +n);
-        return f1(n+1);
+
+    private static int f2(int n) {
+        System.out.println("f2 function: " + n);
+        return f1(n + 1);
     }
+
+    private static void f(int n) {
+        System.out.println(n);
+        if (n == 100) return; // base condition
+        f(n + 1);
+    }
+
+
 }
