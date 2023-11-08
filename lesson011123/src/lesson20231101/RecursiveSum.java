54904a1e31e6f3eeed798cceb26eb30f9bd91556
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-11-08 11:21:30 +0200

    RecursiveSum

--- lesson011123/src/lesson20231101/RecursiveSum.java
+++ lesson011123/src/lesson20231101/RecursiveSum.java
@@ -1,26 +1,13 @@
-package lesson20231101;
+package lesson011123;
 
 public class RecursiveSum {
-
     public static void main(String[] args) {
-
-        int[] data = new int[] {1, 2, 3, 4, 5};
-
-        System.out.println(sumRecursive(data, 0));
-
+        int [] data = new int [] { 1,2,3,4,5};
+        System.out.println(sumRecursive(data,0));
     }
-
-    // Time complexity: O(n)
-    // Space complexity: O(n)
-    public static int sumRecursive(int[] data, int currentIndex) {
+    public static int sumRecursive(int[] data, int currentIndex){
         if (currentIndex == data.length) return 0;
-        int result = data[currentIndex] + sumRecursive(data, currentIndex + 1);
+        int result = data [currentIndex] + sumRecursive(data, currentIndex);
         return result;
     }
-
-
-
-
-
-
 }
